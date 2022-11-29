74ef88d84a4d4ed1516ee6aafc3fdca0c2cf7038
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 <sodoo75@gmail.com>
2022-11-29 22:37:09 +0800

    up

---
+++ day11/ex01/js/ex22.js
@@ -0,0 +1,42 @@
+var doo = ", doo-doo, doo-doo";
+var a = "Baby Shark";
+var b = "Mommy Shark"
+var c = "Daddy Shark"
+var d = "Grandma Shark"
+var e = "Let's go hant"
+var f = "Run away"
+var g = "Safe at last"
+var h = "it's the end"
+
+console.log(a + doo);
+console.log(a + doo);
+console.log(a + doo);
+console.log(a);
+console.log(b + doo);
+console.log(b + doo);
+console.log(b + doo);
+console.log(b);
+console.log(c + doo);
+console.log(c + doo);
+console.log(c + doo);
+console.log(c);
+console.log(d + doo);
+console.log(d + doo);
+console.log(d + doo);
+console.log(d);
+console.log(e + doo);
+console.log(e + doo);
+console.log(e + doo);
+console.log(e);
+console.log(f + doo);
+console.log(f + doo);
+console.log(f + doo);
+console.log(f);
+console.log(g + doo);
+console.log(g + doo);
+console.log(g + doo);
+console.log(g);
+console.log(h + doo);
+console.log(h + doo);
+console.log(h + doo);
+console.log(h);
