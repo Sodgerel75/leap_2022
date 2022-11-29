bc784b9a1bde9334484fe99e5da0826a2394e2d9
Morning <morning@admins-iMac-3.local>
2022-11-29 12:42:07 +0800

    class

---
+++ day11/js/main.js
@@ -0,0 +1,199 @@
+// var a = "Baby Shark, ";
+// var b = "doo-doo, ";
+// var c = "doo-doo"
+// var d = "Mommy Shark, "
+// console.log(a + b + c);
+// console.log(a + b + c);
+// console.log(a + b + c);
+// console.log(a);
+// console.log(d + b + c);
+// console.log(d + b + c);
+// console.log(d + b + c);
+// console.log(d);
+
+
+
+
+
+// let myNumber = 5;
+// let undefy;
+// let nullVariable = null;
+// let myDecision = true;
+
+
+// // typeof
+// console.log(typeof 1);
+// console.log(typeof "Hello");
+// console.log(typeof undify);
+// console.log(typeof nullVariable);
+// console.log(myDecision);
+// console.log(typeof myDecision);
+
+// /**
+//  * alert gargana
+//  * x2+y2-z3 / 240 *175
+//  */
+
+
+// let x = 20;
+// let y = 30;
+// let z = 10;
+
+// // alert((x * x + y * y - z * z * z) / 240 * 175);
+
+
+// // remainder
+// console.log(7 % 2);
+// console.log(36 % 19);
+
+// let result = '30' - 10;
+
+// console.log(result);
+
+
+
+// const PI = 3.14;
+// let r = 5;
+// let circleLength = 2 * PI * r;
+// console.log(circleLength);
+
+
+// let a = 3;
+// let b = 5;
+// let h = 2;
+
+// console.log("Trapezoin Area = " + (a + b) / 2 * h)
+
+
+// let top2 = prompt('top');
+// let bottom2 = prompt('bottom');
+// let height2 = prompt('height');
+
+// console.log("Trapezoin Area = " + (top2 + bottom2) / 2 * height2)
+
+
+// let a = prompt("a utga");
+// let b = prompt("b utga");
+
+
+// if (a < b) {
+//     console.log("it's true");
+// } else {
+//     console.log("it' false");
+// }
+
+
+// const yearOfBirth = prompt("onoo oruul");
+// let old = (2022 - yearOfBirth);
+
+// console.log(old);
+
+// if (0 <= old && old <= 1) {
+//     console.log("Infant")
+// }
+// else if (1 < old && old <= 3) {
+//     console.log("Toddler")
+// }
+// else if (3 < old && old <= 5) {
+//     console.log("Preschool")
+// }
+// else if (5 < old && old <= 12) {
+//     console.log("Gradeschooler")
+// }
+// else if (12 < old && old <= 18) {
+//     console.log("Teen")
+// }
+// else if (18 < old && old <= 21) {
+//     console.log("Young adult")
+// }
+// else {
+//     console.log("Adult")
+// }
+
+
+
+
+
+// let a = prompt("toogoo oruul")
+
+// if (a % 2 == 0) {
+//     console.log("Tegsh")
+// }
+// else {
+//     console.log("Sondgoi")
+// }
+
+
+// let a = prompt("toogoo oruul");
+
+// if (a % 3 == 0 || a % 7 == 0) {
+//     console.log("3 urjver 7 urjver");
+// }
+// else {
+//     console.log("ali ali n bish");
+// }
+
+// let isNotTrue = true;
+// console.log(!isNotTrue)
+
+
+
+
+// let weight = prompt("Jingee oruul");
+// let height = prompt("Unduruu oruul");
+// let bmi = weight / height * height;
+// console.log("BMI rate = " + bmi);
+
+// if (bmi < 18.5) {
+//     console.log("Under Weight")
+// }
+// else if (18.5 < bmi < 24.9) {
+//     console.log("Normal")
+// }
+// else if (25 < bmi < 29.9) {
+//     console.log("Over Weight")
+// }
+// else if (30 < bmi < 34.9) {
+//     console.log("Obesity (Class I)")
+// }
+// else {
+//     console.log("Extreme Obesity")
+// }
+
+
+
+
+
+let day = prompt('Uduruu oruul');
+let dayName;
+let dayNumber = Number(day);
+console.log(dayNumber);
+console.log(typeof (dayNumber))
+
+switch (dayNumber) {
+    case 1:
+        dayName = 'Monday';
+        break;
+    case 2:
+        dayName = 'Tuesday';
+        break;
+    case 3:
+        dayName = 'Wednesday';
+        break;
+    case 4:
+        dayName = 'Thurday';
+        break;
+    case 5:
+        dayName = 'Friday';
+        break;
+    case 6:
+        dayName = 'Saturday';
+        break;
+    case 7:
+        dayName = 'Sunday';
+        break;
+    default:
+        dayName = 'Invalid day';
+}
+console.log(dayName);
+console.log(typeof day);
