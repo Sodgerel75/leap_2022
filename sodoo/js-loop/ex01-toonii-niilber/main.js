474a14ce17872c33e6257bb6e60c475960602f2e
Morning <morning@admins-iMac-3.local>
2022-12-02 08:42:55 +0800

    up

---
+++ sodoo/js-loop/ex01-toonii-niilber/main.js
@@ -0,0 +1,14 @@
+let maxNum = Number(prompt('Gime me number')); // 3
+let count = 0;
+let sum = 0;
+
+while (count <= maxNum) {
+
+    sum = sum + count;
+    console.log(count);
+    count = count + 1;
+}
+
+console.log(sum);
+
+
