90a807e08b44447eb31f931db141ebcd340936f3
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 <sodoo75@gmail.com>
2022-12-04 13:32:47 +0800

    up

--- sodoo/js-loop/ex08-palindromic-number/main.js
+++ sodoo/js-loop/ex08-palindromic-number/main.js
@@ -6,10 +6,16 @@ let i = n.length;
 
 let flipNumber = '';
 
-console.log(i);
+
 console.log(n);
 while (0 < i) {
   flipNumber = flipNumber + n.charAt(i - 1);
   i--;
 }
 console.log(flipNumber);
+
+if (flipNumber == n) {
+  console.log('true');
+} else {
+  console.log('false');
+}
