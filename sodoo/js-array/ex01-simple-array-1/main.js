924dfd26cf7810b07316a70de0a4ba869c927ca6
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 <sodoo75@gmail.com>
2022-12-05 23:05:34 +0800

    up

--- sodoo/js-array/ex01-simple-array-1/main.js
+++ sodoo/js-array/ex01-simple-array-1/main.js
@@ -7,11 +7,11 @@
 
 let itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon',];
 console.log(itCompanies);
-console.log(itCompanies.length);
-console.log(itCompanies[0]);
+console.log('Компаниудын length: ' + itCompanies.length);
+console.log('Эхний компани - ' + itCompanies[0] + ', Сүүлийн компани - ' + itCompanies[6]);
 
 for (let i = 0; i < itCompanies.length; i++) {
-  console.log(itCompanies[i] + ' Good');
+  console.log('Компани - ' + itCompanies[i]);
   itCompanies[i] = itCompanies[i].toUpperCase();
 }
 console.log(itCompanies)
