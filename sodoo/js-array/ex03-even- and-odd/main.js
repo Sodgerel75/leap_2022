61716e4d3218b9ae6cc30c001f06856ef916d235
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 <sodoo75@gmail.com>
2022-12-06 21:56:49 +0800

    update

---
+++ sodoo/js-array/ex03-even- and-odd/main.js
@@ -0,0 +1,14 @@
+let nAll = [5, 6, 4, 12, 19, 121, 1, 7, 9, 63];
+console.log(nAll);
+
+for (let i = 0; i < nAll.length; i++) {
+  if (nAll[i] % 2 == 0) {
+
+    console.log('Even Numbers = ' + nAll[i]);
+  } else {
+    // console.log('Odd Numbers = ' + nAll[i]);
+    // console.log(i);
+  }
+}
+
+// console.log(i);
