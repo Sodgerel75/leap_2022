ee31c2b9b7f71d67ceecb9965f98ac846409d58f
Morning <morning@admins-iMac-3.local>
2022-12-06 08:47:34 +0800

    up

--- sodoo/js-array/ex01-simple-array-1/main.js
+++ sodoo/js-array/ex01-simple-array-1/main.js
@@ -8,7 +8,7 @@
 let itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon',];
 console.log(itCompanies);
 console.log('Компаниудын length: ' + itCompanies.length);
-console.log('Эхний компани - ' + itCompanies[0] + ', Сүүлийн компани - ' + itCompanies[6]);
+console.log('Эхний ком - ' + itCompanies[0] + ', Дунд ком - ' + itCompanies[Math.floor(itCompanies.length / 2)] + ', Сүүлийн ком - ' + itCompanies[itCompanies.length - 1]);
 
 for (let i = 0; i < itCompanies.length; i++) {
   console.log('Компани - ' + itCompanies[i]);
