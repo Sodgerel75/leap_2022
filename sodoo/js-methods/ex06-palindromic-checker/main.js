56d09fe6e717d012dc94647c9d9c7405e9609fb1
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 <sodoo75@gmail.com>
2022-12-08 20:38:29 +0800

    up

---
+++ sodoo/js-methods/ex06-palindromic-checker/main.js
@@ -0,0 +1,59 @@
+
+let n = toArrConverter(prompt("Ta hussen toogoo oruulna uu"));
+// n.reverse();
+console.log(n);
+
+// if (n === n.reverse()) {
+//   console.log(n + ' is palindromic');
+// } else {
+//   console.log(n + ' is not palindromic');
+// }
+
+
+
+
+
+
+function toArrConverter(str) {
+  let words = str.split(' ');
+  return words.join(', ').toLowerCase();
+}
+
+console.log(toArrConverter("123"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let n = prompt("Ta hussen toogoo oruulna uu");
+// let i = n.length;
+
+// let flipNumber = '';
+
+
+// console.log(n);
+// while (0 < i) {
+//   flipNumber = flipNumber + n.charAt(i - 1);
+//   i--;
+// }
+// console.log(flipNumber);
+
+// if (flipNumber == n) {
+//   console.log('true');
+// } else {
+//   console.log('false');
+// }
