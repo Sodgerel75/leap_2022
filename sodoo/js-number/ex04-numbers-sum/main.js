47d1dc54d60735d3d1b43809aa9868b9f9ac74d6
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 <sodoo75@gmail.com>
2022-12-11 21:29:56 +0800

    up

---
+++ sodoo/js-number/ex04-numbers-sum/main.js
@@ -0,0 +1,120 @@
+const users = [
+    {
+        name: 'Alex',
+        email: 'alex@alex.com',
+        skills: ['HTML', 'CSS', 'JavaScript'],
+        age: 45,
+        isLoggedIn: false,
+        points: 30
+    },
+    {
+        name: 'Job',
+        email: 'job@job.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+        age: 25,
+        isLoggedIn: false,
+        points: 50
+    },
+    {
+        name: 'Brook',
+        email: 'daniel@daniel.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+        age: 39,
+        isLoggedIn: true,
+        points: 50
+    },
+    {
+        name: 'Daniel',
+        email: 'daniel@alex.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+        age: 25,
+        isLoggedIn: false,
+        points: 40
+    },
+    {
+        name: 'John',
+        email: 'john@john.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+        age: 20,
+        isLoggedIn: true,
+        points: 50
+    },
+    {
+        name: 'Thomas',
+        email: 'thomas@thomas.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+        age: 18,
+        isLoggedIn: false,
+        points: 40
+    },
+    {
+        name: 'Paul',
+        email: 'paul@paul.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+        age: 25,
+        isLoggedIn: false,
+        points: 40
+    }
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function findGender(array) {
+//     let maleStudents = 0;
+//     let femaleStudents = 0;
+
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i].gender == 'male') {
+//             maleStudents = maleStudents + 1;
+//         } else {
+//             femaleStudents = femaleStudents + 1;
+//         }
+//     }
+//     let genderSort = { male: maleStudents, female: femaleStudents };
+//     return genderSort
+// }
+// // console.log(students);
+// console.log(findGender(students));
+
+
+
+// function findAverageAge(array) {
+//     let ages = 0;
+//     for (let i = 0; i < array.length; i++) {
+//         ages = ages + array[i].age;
+//     }
+//     // console.log(array.length);
+//     // console.log(ages / array.length);
+//     return ages / array.length;
+// }
+
+// console.log('Сурагчдын дундаж нас = ' + findAverageAge(students));
+
+
+
+// function OvogAdd(array) {
+//     for (let i = 0; i < array.length; i++) {
+//         array[i].ovog = 'Боржигон';
+//     }
+//     return array;
+// }
+// console.log(OvogAdd(students));
+
+
+
