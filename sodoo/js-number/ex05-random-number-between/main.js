59f5e36a54afac58cf4b5f2c53401b0df61f04cb
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 <sodoo75@gmail.com>
2022-12-11 21:56:55 +0800

    up

---
+++ sodoo/js-number/ex05-random-number-between/main.js
@@ -0,0 +1,9 @@
+let tooA = Number(prompt('Ehnii toogoo oruul'));
+let tooB = Number(prompt('Daraagiin toogoo oruul'));
+
+let max = Math.max(tooA, tooB);
+let min = Math.min(tooA, tooB);
+console.log('Max too = ' + max);
+console.log('Min too = ' + min);
+
+console.log('Random too = ' + Math.round(Math.random() * (max - min) + min));
