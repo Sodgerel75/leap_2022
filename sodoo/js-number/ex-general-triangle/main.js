2e8d3a2591306729a98657d1dccfe73d70b72fa5
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 <sodoo75@gmail.com>
2022-12-11 22:21:22 +0800

    up

---
+++ sodoo/js-number/ex-general-triangle/main.js
@@ -0,0 +1,6 @@
+// x2 = y2 + z2  x=? input: y=15, z=24
+
+let a = Number(prompt('Gime a'));
+let b = Number(prompt('Gime b'));
+alert('c = ' + Math.round(Math.sqrt((Math.pow(a, 2) + Math.pow(b, 2)))));
+console.log('c = ' + Math.round(Math.sqrt((Math.pow(a, 2) + Math.pow(b, 2)))));
