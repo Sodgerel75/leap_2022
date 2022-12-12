7a677088e9aa5539fb7cbe905cb04a44ec8adef4
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 <sodoo75@gmail.com>
2022-12-12 22:04:41 +0800

    up

--- sodoo/js-loop/ex14-double-loop/main.js
+++ sodoo/js-loop/ex14-double-loop/main.js
@@ -1,3 +1,24 @@
+/*
+1. Давхар давталт 1
+Бүхэл тоо өгөгдөнө. Жишээн дээрхтэй төстэй тоон хэлбэрийг үүсгэ.
+Input: 5
+Output:
+ - 1 
+ - 1 2 
+ - 1 2 3 
+ - 1 2 3 4 
+ - 1 2 3 4 5
+2. Давхар давталт 2
+Бүхэл тоо өгөгдөнө. Жишээн дээрхтэй төстэй тоон хэлбэрийг үүсгэ.
+Input: 5
+Output:
+- 5 4 3 2 1 
+- 5 4 3 2 
+- 5 4 3 
+- 5 4 
+- 5
+*/
+
 console.log('------1. Давхар давталт 1------')
 
 // let maxNum = Number(prompt('Gime me number'));
@@ -7,18 +28,49 @@ let output = '';
 console.log('Input = ' + maxNum);
 while (count <= maxNum) {
     // console.log(count);
-    output = output + ' ' + count;
+    output = output + count + ' ';
     count = count + 1;
     console.log(output);
 }
 // console.log('Давталт - ' + count);
 // console.log('Output = ' + output);
 
+// let number = Number(prompt("Input number"));
+// let sum = "";
 
+// for(let i = 1; i <= number; i++) {
+//     for(let j = 1; j <= i; j++ ) {
+//         sum += j + " ";
+//     }
+//     sum += "\n";
+// }
+// console.log(sum + "\n\n");
 
 
 
 // console.log('------2. Давхар давталт 2------')   // GOYO PATERN GARSAN
+
+
+
+let pattern = "";
+for (let i = 1; i <= maxNum; i++) {
+    for (let j = maxNum; j >= i; j--) {
+        pattern += j + ' ';
+    }
+    pattern += "\n";
+}
+console.log(pattern);
+
+
+
+
+
+
+
+
+
+
+
 
 // // let maxNum = Number(prompt('Gime me number'));
 // let i = 0;
