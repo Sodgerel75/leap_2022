0bad0e95a7435b6fbd96e39aac98b314b8346e32
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 <sodoo75@gmail.com>
2022-12-13 21:18:09 +0800

    up

--- sodoo/js-dom/random-color-generator/main.js
+++ sodoo/js-dom/random-color-generator/main.js
@@ -1,37 +1,29 @@
-setTimeout(() => {
-    console.log('Time out is running');
-}, 5000)
-
-let counter = 0;
-
-// setInterval(startInterval, 1000);
 
 
 
-function runTimerInterval() {
-    window.setInterval(startInterval, 10);
+
+function randomRgbColor() {
+    let r = Math.floor(Math.random() * 255) + 1;;
+    let g = Math.floor(Math.random() * 255) + 1;;
+    let b = Math.floor(Math.random() * 255) + 1;;
+    return [r, g, b];
 }
+let colorCode = randomRgbColor().toString()
+console.log(colorCode)
 
 
-function startInterval() {
-    let now = new Date();
-    let myTimer = document.getElementById('myTimer');
-    // myTimer.innerHTML = now;
-    myTimer.innerHTML = now.getHours() + '-' + now.getMinutes() + '-' + now.getSeconds() + '-' + now.getMilliseconds();
 
-}
-
-let startButton = document.getElementById('starter');
-
-startButton.addEventListener('click', runTimerInterval);
 
 
 
 let colorButton = document.getElementById('colorChanger');
 
 function randomColor() {
-    document.getElementById('bodyCol').style = 'background-color: red';
+    randomRgbColor();
+    document.getElementById('bodyCol').style = `background-color: rgb(${colorCode})`;
+
 }
 
 colorButton.addEventListener('click', randomColor);
+
 
