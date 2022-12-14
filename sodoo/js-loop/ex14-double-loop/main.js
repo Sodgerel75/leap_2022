4d5165af0cf8c8ea13f9fffb313182a27974321e
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 <sodoo75@gmail.com>
2022-12-14 20:32:34 +0800

    up

--- sodoo/js-loop/ex14-double-loop/main.js
+++ sodoo/js-loop/ex14-double-loop/main.js
@@ -21,36 +21,17 @@ Output:
 
 console.log('------1. Давхар давталт 1------')
 
-// let maxNum = Number(prompt('Gime me number'));
-let maxNum = 5;
-let count = 1;
-let output = '';
-console.log('Input = ' + maxNum);
-while (count <= maxNum) {
-    // console.log(count);
-    output = output + count + ' ';
-    count = count + 1;
-    console.log(output);
+let maxNum = Number(prompt('Gime me number'));
+let pattern1 = "";
+for (let i = 1; i <= maxNum; i++) {
+    for (let j = 1; j < i; j++) {
+        pattern1 += j + ' ';
+    }
+    pattern1 += i + "\n";
 }
-// console.log('Давталт - ' + count);
-// console.log('Output = ' + output);
+console.log(pattern1);
 
-// let number = Number(prompt("Input number"));
-// let sum = "";
-
-// for(let i = 1; i <= number; i++) {
-//     for(let j = 1; j <= i; j++ ) {
-//         sum += j + " ";
-//     }
-//     sum += "\n";
-// }
-// console.log(sum + "\n\n");
-
-
-
-// console.log('------2. Давхар давталт 2------')   // GOYO PATERN GARSAN
-
-
+console.log('------2. Давхар давталт 2------')   // GOYO PATERN GARSAN
 
 let pattern = "";
 for (let i = 1; i <= maxNum; i++) {
@@ -62,6 +43,23 @@ for (let i = 1; i <= maxNum; i++) {
 console.log(pattern);
 
 
+document.getElementById('pat').innerHTML = pattern1
+document.getElementById('pat2').innerHTML = pattern
 
 
+
+
+
+
+
+// let maxNum = 5;
+// let count = 1;
+// let output = '';
+// console.log('Input = ' + maxNum);
+// while (count <= maxNum) {
+//     // console.log(count);
+//     output = output + count + ' ';
+//     count = count + 1;
+//     console.log(output);
+// }
 
