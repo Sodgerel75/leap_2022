ff968a784d17035a778ff91bd4e73f57633498a6
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 <sodoo75@gmail.com>
2022-12-14 17:47:12 +0800

    up

--- sodoo/sodoo-to-do-app/main.js
+++ sodoo/sodoo-to-do-app/main.js
@@ -5,20 +5,21 @@ document.getElementById(myTimer.innerHTML = 'Он сар өдөр' + ' ' + now.g
 let task = document.getElementById('list');
 console.log(task.value)
 
-
-
-
-
 let toDoArr = [];
-
 
 function addTask() {
     // document.getElementById('taskList').innerHTML = task.value + 'delete';
     let p = document.createElement('p')
     p.id = toDoArr.length + 1
+    p.style = "col-auto"
 
     // p.innerHTML = task.value + ' <button>edit</button>' + ' <button>done</button>' + ' <button id="del-btn">del</button>';
-    p.innerHTML = `<input value=${task.value} disabled> <button>edit</button> <button>done</button>  <button id="del-btn-${toDoArr.length + 1} " onclick="delP()">del</button>`;
+    p.innerHTML = `
+    <input value=${task.value} disabled> 
+    <button  class="btn btn-warning"><i class="bi bi-pencil"></i></button> 
+    <button  class="btn btn-success"><i class="bi bi-check-circle"></i></button>  
+    <button id="del-btn-${toDoArr.length + 1} " onclick="delP()" class="btn btn-danger"><i class="bi bi-trash"></i></button>
+    `;
     toDoArr.push(task.value)
     console.log(toDoArr);
 
@@ -29,26 +30,6 @@ function addTask() {
 
     // const element = document.getElementById("container");
     // element.appendChild(listAdded);
-
-
-
-
-
-
-    // let delButton = document.getElementById('del-btn-1');
-
-    // delButton.addEventListener('click', delP);
-
-
-    // function delP() {
-    //     const element = document.getElementById('1');
-    //     // document.getElementById('1').remove();
-    //     element.remove();
-    //     console.log('clicked');
-    //     document.getElementById('1').remove;
-    // }
-
-
 }
 
 let button = document.getElementById('add');
@@ -58,22 +39,19 @@ button.addEventListener('click', addTask);
 
 
 
-
-
-
-
-
 // delete function
 
-let delButton = document.getElementById('del-btn-1');
+// let delButton = document.getElementById('del-btn-1');
 
-delButton.addEventListener('click', delP);
+// delButton.addEventListener('click', delP);
 
 
 function delP() {
     console.log('deleted');
     // const element = document.getElementById('1');
+    console.log(toDoArr.length);
     document.getElementById('1').remove();
+    // document.getElementById(`${toDoArr.length}`).remove();
     // element.removeChild(list.firstElementChild);
 }
 
