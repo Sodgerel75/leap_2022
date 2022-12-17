551e0c9f6469ea15305bbbf9c2a4893ccc70d55d
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 <sodoo75@gmail.com>
2022-12-17 22:07:39 +0800

    u

--- sodoo/js-string/simple-assingment/main.js
+++ sodoo/js-string/simple-assingment/main.js
@@ -1,1 +1,23 @@
-console.log(`Hello I am "Name Here". \nI am 'Age here' years old. \n    This is the new line with tab and some \\backslashes\\.`);
+let fName = 'Sodgerel';
+let lName = 'Gansukh';
+
+if (fName.length > lName.length) {
+  console.log(fName + ' нь ' + lName + ' -ээсээ илүү урт');
+}
+
+console.log(fName.toLocaleLowerCase());
+
+console.log(lName.toLocaleUpperCase());
+
+let text1 = 'Pinecone academy- н';
+let text2 = text1.concat('', ' leap хөтөлбөрт тавтай морилго уу? ');
+console.log(text2);
+
+let city = 'Улаанбаатар'
+let district = 'Сүхбаатар'
+
+console.log('Hi, намайг ' + lName + ' овогтой ' + fName + ' гэдэг.' + '\nБи ' + city + ' хотын ' + district + ' дүүрэгт амьдардаг.');
+
+
+
+
