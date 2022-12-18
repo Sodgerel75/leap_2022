3289280ce0ffc451d92523ba03fdd1483254758a
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 <sodoo75@gmail.com>
2022-12-19 02:49:03 +0800

    up

---
+++ sodoo/js-dom/click-remove-from-dropdown/main.js
@@ -0,0 +1,37 @@
+let delButton = document.getElementById('del-btn');
+let num = document.getElementById('colorSelect');
+
+console.log(num);
+
+function delList() {
+
+}
+
+
+
+
+
+
+
+
+// let delBtn = document.getElementById("btnDel");
+// let colorsItem = document.getElementById("selectItem");
+// let displaySee = document.getElementById("listItem");
+
+// function getDomList() {
+//   console.log(colorsItem.length);
+//   for (const element of colorsItem) {
+//     console.log(element.innerText);
+//   }
+// }
+
+// function deleteColor() {
+//   let selectIndex = colorsItem.selectedIndex;
+//   colorsItem.remove(selectIndex);
+// }
+
+// function getSelectValue() {
+//   let selectedValue = colorsItem.value;
+//   console.log("Selected: " + selectedValue);
+// }
+// getDomList();
