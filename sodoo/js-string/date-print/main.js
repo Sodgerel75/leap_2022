62c58fa18e39f596da9c8188ef5a2e9801ae56bf
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 <sodoo75@gmail.com>
2022-12-18 16:50:33 +0800

    u

--- sodoo/js-string/date-print/main.js
+++ sodoo/js-string/date-print/main.js
@@ -1,3 +1,6 @@
 const jiguurten = ['Elee', 'Heree', 'Galuu'];
 
 console.log(jiguurten.join('-')); // "Elee-Heree-Galuu"
+
+const ognoo = prompt('Ognoo oruulna uu');
+console.log(ognoo.replaceAll(', ', '-'));
