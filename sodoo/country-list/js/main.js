a5071f37ff3a9facb85de199a65fb5896e76360e
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 <sodoo75@gmail.com>
2022-12-27 20:42:40 +0800

    up

--- sodoo/country-list/js/main.js
+++ sodoo/country-list/js/main.js
@@ -1,25 +1,51 @@
+let data = [];
+
+fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then((country) => {
+        data = country
+        console.log(data);
+        // console.log(country[5].continents[0]);
+
+        // country.map((element) => {
+        //     console.log(country[element].continents[0]);
+        // })
+
+        // ----------------------- Render Code ------------------------
+        const conAllDom = document.getElementById('conAll');
+        conAllDom.innerHTML = '';
+        country.map((element) => {
+            conAllDom.innerHTML += getCard(element)
+        })
+        // ----------------------- Render Code ------------------------
+    })
+
+
+
+
+
+
+
 async function searchFunc(event) {
     const searchVal = document.getElementById('input').value;
     console.log(searchVal);
 
-    const country = await fetch('https://restcountries.com/v3.1/all');
-    const countryJSON = await country.json();
-    console.log(countryJSON);
+    // const country = await fetch('https://restcountries.com/v3.1/all');
+    // const countryJSON = await country.json();
+    // console.log(countryJSON);
 
-    // const animesData = countryJSON.name;
-    // console.log(animesData);
-
-    const searchResult = countryJSON.filter(el =>
+    const searchResult = data.filter(el =>
         el.name.common.toLowerCase().includes(searchVal.toLowerCase())
     )
     console.log(searchResult);
 
+    // ----------------------- Render Code ------------------------
     const conAllDom = document.getElementById('conAll');
-    let result = ''
-    searchResult.map((element, index) => {
-        result += getCard(element, index);
+    conAllDom.innerHTML = '';
+    searchResult.map((element) => {
+        conAllDom.innerHTML += getCard(element)
     })
-    conAllDom.innerHTML = result;
+    // ----------------------- Render Code ------------------------
 }
 
 
@@ -35,20 +61,3 @@ function getCard(data) {
     `
 }
 
-fetch('https://restcountries.com/v3.1/all')
-    .then(res => res.json())
-    .then((country) => {
-        // console.log(country);
-        // console.log(country[5].continents[0]);
-
-        // country.map((element) => {
-        //     console.log(country[element].continents[0]);
-        // })
-
-        const conAllDom = document.getElementById('conAll');
-
-        // conAllDom.innerHTML = '';
-        country.map((element) => {
-            conAllDom.innerHTML += getCard(element)
-        })
-    })
