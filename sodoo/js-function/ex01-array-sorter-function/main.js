0bbb62c589a1722e74fbc269f50632d8c4f88f42
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 <sodoo75@gmail.com>
2023-01-14 22:40:04 +0800

    up

--- sodoo/js-function/ex01-array-sorter-function/main.js
+++ sodoo/js-function/ex01-array-sorter-function/main.js
@@ -1,19 +1,31 @@
-sortArray = [1, 2, 8, 7, -1, 10];
+sortArray = [4, 32, 2, 5, 8, -10, 20, 1, 0, -20];
 
 function sortingAlgorithm(arr) {
+    // arr.sort((a, b) => a - b)
 
-    for (let i = 0; i < arr.length - 2; i++) {
-        let indexMin = i;
-        for (let j = i + 1; j < arr.length - 1; j++); {
-            if (arr[j] < arr[indexMin]) {
-                indexMin = j;
-            }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > arr[i + 1]) {
+            let temp = arr[i];
+            arr[i] = arr[i + 1]
+            arr[i + 1] = temp;
+            i = -1;
         }
-        return arr[i];
-        arr[i] = arr[indexMin];
-        arr[indexMin] = temp;
 
     }
+
+    // for (let i = 0; i < arr.length - 2; i++) {
+    //     let indexMin = i;
+    //     for (let j = i + 1; j < arr.length - 1; j++); {
+    //         if (arr[j] < arr[indexMin]) {
+    //             indexMin = j;
+    //         }
+    //     }
+    //     return arr[i];
+    //     arr[i] = arr[indexMin];
+    //     arr[indexMin] = temp;
+
+    // }
     return arr;
 }
 
