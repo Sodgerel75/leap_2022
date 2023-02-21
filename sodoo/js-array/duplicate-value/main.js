cc944d0cabf31930c0e552ec4ed191f9a35af0c4
sodoo22 <sodoo75@gmail.com>
2023-02-21 20:56:31 +0800

    find-duplicated-value

--- sodoo/js-array/duplicate-value/main.js
+++ sodoo/js-array/duplicate-value/main.js
@@ -1,2 +1,29 @@
 // Input: [4, 2, 34, 4, 1, 12, 1, 4]
 // Output: [4, 1]
+
+let arr = [4, 2, 34, 4, 1, 12, 1, 4];
+
+let duplicates = [];
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr.length; j++) {
+    if (i !== j) {
+      if (!duplicates.includes(arr[i])) {
+        if (arr[i] == arr[j]) {
+          duplicates.push(arr[i]);
+        }
+      }
+    }
+  }
+}
+console.log(duplicates);
+
+// const findDuplicates = (arry) =>
+//   arry.filter((item, index) => arry.indexOf(item) !== index);
+
+// console.log(findDuplicates(arr));
+
+// let uniqueNumber = duplicates.filter((c, index) => {
+//   return duplicates.indexOf(c) === index;
+// });
+
+// console.log(uniqueNumber);
