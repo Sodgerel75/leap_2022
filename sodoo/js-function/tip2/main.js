4e43661d10b64c4b9fea7d9b0d79f605901f7e08
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 <sodoo75@gmail.com>
2023-02-23 23:46:23 +0800

    tip-done

--- sodoo/js-function/tip2/main.js
+++ sodoo/js-function/tip2/main.js
@@ -1,4 +1,4 @@
-function tip(money) {
+function bill(money) {
   if (5000 <= money && money <= 30000) {
     return money + money * 0.15;
   } else {
@@ -6,4 +6,24 @@ function tip(money) {
   }
 }
 
+console.log(bill(3000));
+console.log(bill(27500));
+console.log(bill(100000));
+
+function tip(money) {
+  if (5000 <= money && money <= 30000) {
+    return money * 0.15;
+  } else {
+    return money * 0.2;
+  }
+}
+
 console.log(tip(3000));
+console.log(tip(27500));
+console.log(tip(100000));
+
+function average(a, b, c) {
+  return (a + b + c / 3).toFixed();
+}
+
+console.log("Average :", average(3000, 27500, 100000));
